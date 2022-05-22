7fb20dda9710e69cdd7b31356e76a66b27d7e2fa
pherreram1600@alumno.ipn.mx <$1st3wasH>
2022-05-21 21:22:34 -0500

    se ha desecho la funcion len, dado que apuntadores se require otro tipo de procesamiento, en vez de ello, se aporto por pasar la lonitud de los arreglos

--- main.c
+++ main.c
@@ -47,18 +47,10 @@ int * subarray(int * arr,int start, int max){
     return sub;
 }
 
-/**
- * retorna la longitud de un arreglo dinamico
- * @param arr
- * @return
- */
-int len(int * arr){
-    return  sizeof(arr) / sizeof(arr[0]);
-}
 
-int sum(int * arr){
+int sum(int * arr,int n){
     int sum = 0;
-    for(int i = 0; i < len(arr); i++){
+    for(int i = 0; i < n; i++){
         sum += arr[i];
     }
     return sum;
@@ -69,10 +61,10 @@ int * middlecalc(int  * arr,int mitad){
     return arr;
 }
 
-int * maxarray(int * left,int * middle,int * right){
-    int sumleft = sum(left);
-    int summiddle = sum(middle);
-    int sumrigth = sum(right);
+int * maxarray(int * left,int * middle,int * right,int n){
+    int sumleft = sum(left,n);
+    int summiddle = sum(middle,n);
+    int sumrigth = sum(right,n);
 
     if(sumleft > summiddle){
         if(sumleft > sumrigth){
@@ -86,38 +78,40 @@ int * maxarray(int * left,int * middle,int * right){
     return middle;
 }
 
-int * vectormax(int * arr){
-    // caso mas basico, cuando tiene solo eleento
-    if(len(arr) == 1){
+int * vectormax(int * arr,int n){
+    // caso mas basico, cuando tiene solo elemento
+
+    if(n == 1){
         // caso base
         return arr; // cuando es un solo elemento el elementos mas grande es el mismo.
     }
     // dividimos el problema en mas pequeños
-    int middle = len(arr) / 2;
+    int middle = n / 2;
+    // daddo que tenemos en redondeo, si tenemos residuo significa que que tenemos un arreglo impar
+    // dado al redondeo de la de la division entera, perdemos un elemento del arreglo izquierdo, es por
+    // que verificamo este caso para agregarselo a la longitud del arreglo derecho
+    int lenrigth = (n % 2) == 0 ? middle : middle + 1;
+
+
     int * arrayleft = subarray(arr, 0, middle);
-    int * arrayright = subarray(arr, middle, len(arr));
+    int * arrayright = subarray(arr, middle,n);
 
     // mandamos a llamar a si mismo para eocntrar la solucion
     // sin embargo, tenemos que considerar  que tenemos que tres posibles soluciones
     // uno donde el maximo este a la izquierda y otro donde este a la derehca,
     // por utlimo tenemos el caso donde se ecuentre en medio
-    int * maxleft = vectormax(arrayleft);
-    int * maxright = vectormax(arrayright);
+    int * maxleft = vectormax(arrayleft,middle);
+    int * maxright = vectormax(arrayright,lenrigth);
     //int * maxmiddle = middlecalc(arr, middle);
     // por ultimo combinamos retornarno el arreglo maximo
     //return maxarray(maxleft,maxmiddle,maxright);
+
 }
 
 int main() {
     int n = 10;
     int * arr = newarray(n);
-
-    int * max = vectormax(arr);
-
-    printf("Arreglo original");
-    printarray(arr,n);
-    printf("\nSub arreglo mas grande\n");
-    //printarray(max,len(max));
+    int * max = vectormax(arr,n);
 
     return 0;
 }
